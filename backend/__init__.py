cb29f68ac311caeb61eec98952ee1139a44bcd5b
gadgilsn1003 <gadgilsn@mail.uc.edu>
2026-07-11 14:11:29 -0400

    Add extract_insights_from_notes, search_kols, get_saved_articles exports

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,9 +1,9 @@
 # MSL AI Copilot - Backend Package
 
 from .pubmed_fetcher import search_pubmed, fetch_articles, search_and_fetch
-from .llm_engine import summarize_article, generate_kol_briefing, extract_insights, build_rag_index, ask_literature
+from .llm_engine import summarize_article, generate_kol_briefing, extract_insights, build_rag_index, ask_literature, extract_insights_from_notes
 from .compliance_filter import scan_text, get_compliance_badge
-from .database import init_db, save_article, get_all_kols, add_kol, log_interaction, get_all_interactions, get_interaction_stats
+from .database import init_db, save_article, get_all_kols, add_kol, log_interaction, get_all_interactions, get_interaction_stats, search_kols, get_saved_articles
 
 __all__ = [
     "search_pubmed",
@@ -14,6 +14,7 @@ __all__ = [
     "extract_insights",
     "build_rag_index",
     "ask_literature",
+    "extract_insights_from_notes",
     "scan_text",
     "get_compliance_badge",
     "init_db",
@@ -23,4 +24,6 @@ __all__ = [
     "log_interaction",
     "get_all_interactions",
     "get_interaction_stats",
+    "search_kols",
+    "get_saved_articles",
 ]
